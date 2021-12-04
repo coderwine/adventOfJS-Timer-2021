e59a158aed436905ef955312e7fe83ed5abda0b1
Eric <ejwinebrenner@gmail.com>
2021-12-04 15:59:01 -0500

    Timer is working - still WIP

--- STARTER-FILES/scripts/index.js
+++ STARTER-FILES/scripts/index.js
@@ -1,69 +1,117 @@
 /*
-- Start the timer by clicking on the start link/button. (done)
-- Once the user clicks start, the word start will change to stop. Then, the user can click on the stop button to make the timer stop. (WIP)
-- Click on the gear icon to change the length (minutes and seconds) of the timer. (Need built)
-- Once the timer finishes, the ring should change from green to red and an alert message is passed to the browser. (Need Built)
+- Start the timer by clicking on the start link/button. (DONE)
+- Once the user clicks start, the word start will change to stop. Then, the user can click on the stop button to make the timer stop. (DONE)
+- Click on the gear icon to change the length (minutes and seconds) of the timer. (WIP)
+- Once the timer finishes, the ring should change from green to red and an alert message is passed to the browser. (DONE)
 */
 
-const trigger = document.querySelector('.start');
+const settingOne = document.querySelector('button');
 const min = document.querySelector('.minutes').children[0];
 const sec = document.querySelector('.seconds').children[0];
-let stopper;
+const ring = document.querySelector('.ring');
+const gear = document.querySelector('.settings');
 
-trigger.addEventListener('click', checkStatus);
-// stopper.addEventListener('click', stopFunction);
+settingOne.addEventListener('click',checkStatus);
+gear.addEventListener('click', setNewTime);
 
 let minutes = min.value;
 let seconds = sec.value;
-// let timer;
+let timer;
+let position = `start`;
 
+//! CHECK STATE
 function checkStatus() {
-    console.log(trigger.id);
-    let id = trigger.id
     
-    switch(id) {
-        case id === 'stop':
-            stopFunction();
-            break;
-        case id === 'start':
-            countDown();
-            break;
-        default :
-            countDown();    
+    // console.log('CHECK', position)
+
+    if (position === 'start') {
+        countDown();
+    } else {
+        stopFunction();
     }
 
 }
 
+//! START
 function countDown() {
-    console.log('Count Down!')
-    
-    let timer = setInterval(sandTimer,1000);
+    // console.log('Count Down!')
+    timer = setInterval(sandTimer,1000);
+}
 
-    trigger.innerText = 'stop';
-    trigger.setAttribute('id', 'stop');
-    stopper = document.getElementById('stop').addEventListener('click', stopFunction);
-
-    // setInterval(() => {
-    function sandTimer() {
-        if(seconds == 00) {
+//! TIMER VALUE
+function sandTimer() {
+    if(seconds == 00) {
+        if (min.value == 00 && sec.value == 00) {
+            ring.className = 'ring ending circle'
+            stopFunction();
+        } else {
             min.value = minutes-1;
             seconds = 59
-        } else {
-            seconds-=1;
         }
-        console.log(seconds)
-        sec.value = seconds;
-    }
-    // },1000)
+    } else {
+        sec.value.length === 1 ? 
+            seconds = `0${(seconds - 1)}` :
+            sec.value[0] == 0 || sec.value == 10 ?
+            seconds = `0${(seconds - 1)}` :
+            seconds -= 1;
 
-    function stopFunction () {
-        console.log('Stop Timer!')
-    
-        clearInterval(timer);
-    
-        trigger.innerText = 'start';
-        trigger.setAttribute('id', 'start');
     }
+
+    sec.value = seconds.toString();
+    position = 'stop'
 }
 
-//NOTE Status - counting down.  Need to fix the stop, add a zero to single digit seconds, add styling for when the timer gets to zero and set up the gear.
+//! STOP  
+function stopFunction () {
+    clearInterval(timer);
+    position = `start`;
+}
+
+function setNewTime () {
+    // Create Form
+    const wrapper = document.querySelector('.wrapper');
+    const div = document.createElement('div');
+    const form = document.createElement('form');
+    const p = document.createElement('p');
+    const inputOne = document.createElement('input');
+    const inputTwo = document.createElement('input');
+    const btn = document.createElement('button');
+    const close = document.createElement('button');
+    
+    // Attributes to Form
+    div.style = `
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+        align-items: center;
+        height: 200px; 
+        width: 500px; 
+        background-color: rgba(0,0,0,.5); 
+        position: fixed; 
+        bottom: 10px;
+        border: .01em solid white;
+        border-radius: 10px;
+        `
+    form.setAttribute('onSubmit', (event) => changeTime(event))
+        btn.setAttribute('type', 'submit')
+        btn.innerText = 'Update Time'
+        close.innerText = 'Cancel'
+
+    // Build Form 
+    form.appendChild(p)
+    form.appendChild(inputOne)
+    form.appendChild(inputTwo)
+    form.appendChild(close)
+    form.appendChild(btn)
+    div.appendChild(form);
+    wrapper.appendChild(div);
+    console.log(form)
+}
+
+function changeTime(event) {    
+    event.preventDefault();
+    console.log('Changed Time HIT')
+}
+
+
+//NOTE : Need to build user input of custom timer.
